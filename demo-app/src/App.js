7669e9cb7c04c1816f6c2d8973292fed14aec17b
jalvsaker <ec2-user@ip-172-31-40-157.eu-west-1.compute.internal>
2023-10-17 09:19:15 +0000

    hei

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [hei, setHei] = useState(0);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> DevOpsForLife !
-        </p>
-        <p>YOOOOOO!!!!!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setHei(hei+1)}>{hei}</button>
     </div>
   );
 }
